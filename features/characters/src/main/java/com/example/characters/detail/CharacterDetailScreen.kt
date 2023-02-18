56fd0fbd870cdb71cd3da5c2f176dfedcc72df01
Masoud <masoud.khoshkamkhatibgourabi@snapp.cab>
2023-02-18 19:05:11 +0330

    Set SplashActivity as start activity

--- features/characters/src/main/java/com/example/characters/detail/CharacterDetailScreen.kt
+++ features/characters/src/main/java/com/example/characters/detail/CharacterDetailScreen.kt
@@ -1,12 +1,13 @@
 package com.example.characters.detail
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination(start = true)
 @Composable
 fun CharacterDetailScreen(
-
+    modifier: Modifier = Modifier,
 ) {
 
 }
--- features/home/src/main/java/com/example/home/HomeScreen.kt
+++ features/home/src/main/java/com/example/home/HomeScreen.kt
@@ -1,10 +1,13 @@
 package com.example.home
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination(start = true)
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
 
 }
